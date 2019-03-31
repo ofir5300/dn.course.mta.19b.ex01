278c7849a8032c63bdf711361189f37454c1ce78
Ilan Kushnir <ilanku@mta.ac.il>
2019-04-01 01:35:13 +0300

    Q2 ready - before off's QA

--- B19_Ex01_01/Program.cs
+++ B19_Ex01_01/Program.cs
@@ -9,16 +9,6 @@ namespace B19_Ex01_01
     {
         public static void Main()
         {
-            // functions: 
-            /* private static int[] getInputFromUser
-             * private int convertBinaryToDecimal(int i_binNumber)
-        private static int calculateAvarageDigitAppearances(String[] i_binNumbersArr, int i_digit)
-             * private int countPowerOf2Numbers(int[] i_binNumbersArr)
-             * private int calculateDecimalAvarage(int[] i_decNumbersArr)
-             * private bool checkValidInput(int i_input)
-             * private static bool isNumberAnIncreasingSeries(int i_number)
-
-             */
             string[] i_binaryInputArray = getInputFromUser();
             int[] i_decimalArray = new int[i_binaryInputArray.Length];
             int i_numberOfAscendingSerieses = 0;
--- B19_Ex01_02/Program.cs
+++ B19_Ex01_02/Program.cs
@@ -9,7 +9,61 @@ namespace B19_Ex01_02
     {
         public static void Main()
         {
-            Console.WriteLine
+            String i_input;
+            int i_clockSize;
+
+            Console.WriteLine("Please Enter the sand clock size: ");
+            i_input = Console.ReadLine();
+
+            while(!int.TryParse(i_input, out i_clockSize))
+            {
+                Console.WriteLine("Please Enter a decimal number: ");
+                i_input = Console.ReadLine();
+            }
+
+            printTree(i_clockSize, i_clockSize);
+            Console.ReadLine();
+        }
+
+        private static void printTree(int i_toPrint, int i_initialSize)
+        {
+            if (i_toPrint <= 0)
+            {
+                return;
+            }
+
+            // Top part
+            printClockLine(i_toPrint, i_initialSize);
+            // Recursive call
+            printTree(i_toPrint - 2, i_initialSize);
+            // Bottom part
+            printClockLine(i_toPrint, i_initialSize);
+        }
+
+        private static void printClockLine(int i_toPrint, int i_initialSize)
+        {
+            int i_oneSideSpaces = (i_initialSize - i_toPrint) / 2;
+            int i;
+
+            // left side spaces
+            for (i = 0; i < i_oneSideSpaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            // print *
+            for (i = 0; i < i_toPrint; i++)
+            {
+                Console.Write("*");
+            }
+
+            // right side spaces
+            for (i = 0; i < i_oneSideSpaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("");
         }
     }
 }
