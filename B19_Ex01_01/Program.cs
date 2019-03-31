d17f4a828b6e540ad0a7e59a06864faf1aff98ea
Ofir Cohen <ofirch@mta.ac.il>
2019-03-31 19:03:57 +0300

    q1 functions declared

--- B19_Ex01_01/Program.cs
+++ B19_Ex01_01/Program.cs
@@ -9,7 +9,14 @@ namespace B19_Ex01_01
     {
         public static void Main()
         {
-            // ilan ah shelii
+            // functions: 
+            /* private static int[] getInputFromUser
+             * private int convertBinaryToDecimal(int i_binNumber)
+             * private int calculateAvarageDigitAppearances(int[] i_binNumbersArr)
+             * private int countPowerOf2Numbers(int[] i_binNumbersArr)
+             * private int calculateDecimalAvarage(int[] i_decNumbersArr)
+             * private bool checkValidInput(int i_input)
+             */
         }
     }
 }
