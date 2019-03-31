ef34cb677ba828bc5ab1534734c75cc1defc2f77
Ofir Cohen <ofirch@mta.ac.il>
2019-03-31 19:08:13 +0300

    test 4

--- B19_Ex01_01/Program.cs
+++ B19_Ex01_01/Program.cs
@@ -10,6 +10,7 @@ namespace B19_Ex01_01
         public static void Main()
         {
             // functions: 
+            //off
             /* private static int[] getInputFromUser
              * private int convertBinaryToDecimal(int i_binNumber)
              * private int calculateAvarageDigitAppearances(int[] i_binNumbersArr)
@@ -17,6 +18,12 @@ namespace B19_Ex01_01
              * private int calculateDecimalAvarage(int[] i_decNumbersArr)
              * private bool checkValidInput(int i_input)
              */
+
+        }
+
+        private static int[] getInputFromUser()
+        {
+            int[] o_inputArray;
         }
     }
 }
