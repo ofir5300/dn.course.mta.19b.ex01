dabf7a84c2a23796f303fe70faf489385f4bba11
Ilan Kushnir <ilanku@mta.ac.il>
2019-04-01 00:56:01 +0300

    convert fixes

--- B19_Ex01_01/Program.cs
+++ B19_Ex01_01/Program.cs
@@ -17,7 +17,7 @@ namespace B19_Ex01_01
              * private int calculateDecimalAvarage(int[] i_decNumbersArr)
              * private bool checkValidInput(int i_input)
              */
-            getInputFromUser();
+            Console.WriteLine(convertBinaryToDecimal("00000011"));
         }
 
         private static string[] getInputFromUser()
@@ -62,14 +62,14 @@ namespace B19_Ex01_01
             return true;
         }
 
-        private int convertBinaryToDecimal(String i_binNumber)
+        private static int convertBinaryToDecimal(String i_binNumber)
            { 
             int o_decNumber = 0;
-            for (int i = 0; i < 8; i++)
+            for (int i = 7; i >= 0; i--)
             {
                 if(i_binNumber[i] == '1')
                 {
-                    o_decNumber += (int)(Math.Pow(2, i));
+                    o_decNumber += (int)(Math.Pow(2, 7 - i));
                 }
             }
 
@@ -84,7 +84,7 @@ namespace B19_Ex01_01
             {
                 for(int j = 0; j < 8; j++)
                 {
-                    if (i_binNumbersArr[i][j] == Convert.ToChar(i_digit)
+                    if (i_binNumbersArr[i][j] == Convert.ToChar(i_digit))
                     {
                         digitCount++;
                     }
---
+++ B19_Ex01_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B19_Ex01_02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine
+        }
+    }
+}
