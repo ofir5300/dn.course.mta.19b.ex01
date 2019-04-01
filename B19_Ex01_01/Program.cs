a1a7a23d8fe63e0344cf788b57cb92ee03d01c67
Ofir Cohen <ofirch@mta.ac.il>
2019-04-01 16:41:15 +0300

    q4 declerations

--- B19_Ex01_01/Program.cs
+++ B19_Ex01_01/Program.cs
@@ -9,53 +9,153 @@ namespace B19_Ex01_01
     {
         public static void Main()
         {
-            // functions: 
-            /* private static int[] getInputFromUser
-             * private int convertBinaryToDecimal(int i_binNumber)
-             * private int calculateAvarageDigitAppearances(int[] i_binNumbersArr)
-             * private int countPowerOf2Numbers(int[] i_binNumbersArr)
-             * private int calculateDecimalAvarage(int[] i_decNumbersArr)
-             * private bool checkValidInput(int i_input)
-             */
+            string[] i_binaryInputArray = getInputFromUser();
+            int[] i_decimalArray = new int[i_binaryInputArray.Length];
+            int i_numberOfAscendingSerieses = 0;
 
+            Console.WriteLine("The numbers you entered in decimal form are : ");
+            for (int i = 0; i < i_binaryInputArray.Length; i++)
+            {
+                Console.WriteLine(i_decimalArray[i] = convertBinaryToDecimal(i_binaryInputArray[i]));
+            }
+            Console.WriteLine("Avarage apearences of 0's in each binary number : {0}\n" +
+                    "Avarage apearences of 1's in each binary number : {1}",
+                    calculateAvarageDigitAppearances(i_binaryInputArray, 0), calculateAvarageDigitAppearances(i_binaryInputArray, 1));
+            Console.WriteLine("The count of numbers which is a power of 2 is : " + countPowerOf2Numbers(i_decimalArray));
+            for (int i = 0; i < i_decimalArray.Length; i++)
+            {
+                if (isNumberAnAscendingSeries(i_decimalArray[i]))
+                {
+                    i_numberOfAscendingSerieses++;
+                }
+            }
+            Console.WriteLine("The count of numbers which is a ascending series is : " + i_numberOfAscendingSerieses);
+            Console.WriteLine("The avarage of the given numers is : " + calculateDecimalAvarage(i_decimalArray));
+            Console.ReadLine();
         }
 
-        private int convertBinaryToDecimal(int i_binNumber)
+        private static string[] getInputFromUser()
         {
-            int[] o_inputArray = new int[8];
-            int i_inputCounter = 0;
+            string[] o_inputArray = new string[4];
+            string i_input;
+            int i_inputCounter;
             bool v_isValidInput = true;
 
             Console.WriteLine("Please enter 4 binary numbers with 8 digits each");
-            do
+            for (i_inputCounter = 0; i_inputCounter < 4; i_inputCounter++)
             {
-                Console.WriteLine("please enter value number {0}", i_inputCounter);     // input +1
-                o_inputArray[i_inputCounter] = Console.ReadLine();
-
-            } while (!v_isValidInput);
+                do
+                {
+                    Console.WriteLine("please enter value number {0}: ", i_inputCounter + 1);
+                    i_input = Console.ReadLine();
+                    v_isValidInput = checkValidInput(i_input);
+                } while (!v_isValidInput);                      // procceed only if the current input is valid
+                o_inputArray[i_inputCounter] = i_input;
+            }
 
             return o_inputArray;
         }
 
-        private int convertBinaryToDecimal(int i_binNumber)
+        private static bool checkValidInput(string i_input)
         {
-            int o_decNumber = 0;
-            for (int i = 0; i < 8; i++)
+            if (i_input.Length != 8)
             {
-                o_decNumber += (i_binNumber % 10) * (int)(Math.Pow(2, i));
-                i_binNumber /= 10;
+                Console.WriteLine("The number has to be 8 digits length! Please insert a valid number");
+                return false;
             }
 
-            return o_decNumber;
-            int o_decNumber = 0;
             for (int i = 0; i < 8; i++)
             {
-                o_decNumber += (i_binNumber % 10) * (int)(Math.Pow(2, i));
-                i_binNumber /= 10;
+                if (i_input[i] != '0' && i_input[i] != '1')
+                {
+                    Console.WriteLine("All digit must be 1 or 0 in a binary number! Please insert a valid number");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int convertBinaryToDecimal(String i_binNumber)
+        {
+            int o_decNumber = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                if (i_binNumber[i] == '1')
+                {
+                    o_decNumber += (int)(Math.Pow(2, 7 - i));
+                }
             }
 
             return o_decNumber;
         }
 
+        private static int calculateAvarageDigitAppearances(String[] i_binNumbersArr, int i_digit)
+        {
+            int i_digitCount = 0, i_average;
+
+
+            for (int i = 0; i < i_binNumbersArr.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i_binNumbersArr[i][j] == i_digit + '0')
+                    {
+                        i_digitCount++;
+                    }
+                }
+            }
+
+            i_average = i_digitCount / i_binNumbersArr.Length;
+
+            return i_average;
+        }
+
+        private static bool isNumberAnAscendingSeries(int i_number)
+        {
+            int i_LSB;
+
+            while (i_number != 0)
+            {
+                i_LSB = i_number % 10;
+                i_number /= 10;
+
+                if (i_LSB <= i_number % 10)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int calculateDecimalAvarage(int[] i_decNumbersArr)
+        {
+            int i_sum = 0;
+
+            for (int i = 0; i < i_decNumbersArr.Length; i++)
+            {
+                i_sum += i_decNumbersArr[i];
+            }
+
+            return i_sum / i_decNumbersArr.Length;
+        }
+
+
+
+        private static int countPowerOf2Numbers(int[] i_decNumbersArr)
+        {
+            int o_counter = 0;
+
+            for (int i = 0; i < i_decNumbersArr.Length; i++)
+            {
+                if ((int)Math.Sqrt(i_decNumbersArr[i]) == (double)Math.Sqrt(i_decNumbersArr[i]))
+                {
+                    o_counter++;
+                }
+            }
+
+            return o_counter;
+        }
     }
 }
--- B19_Ex01_04/Program.cs
+++ B19_Ex01_04/Program.cs
@@ -10,6 +10,27 @@ namespace B19_Ex01_04
         public static void Main()
         {
 
+            /*
+             * private static String getInputFromUser()
+             * private static bool checkValidInput(String i_input)
+             * private static bool isPalyndrom(String i_input)
+             * private static bool isDivisibleBy3(String i_input)
+             * private static int countLowerCaseLetters(String i_input)
+             * 
+             */
+
+        }
+
+        private static bool isDivisibleBy3(String i_input)
+        {
+            int i_decimalInput;
+
+            if(!int.TryParse(i_input, out i_decimalInput))              // if the string cannot be convert to int it cannot by divide by 3
+            {
+                return false;
+            }
+
+
         }
     }
 }
