e598bfb6442d98802eb2e708e9fa6302c8c71334
Ofir Cohen <ofirch@mta.ac.il>
2019-04-01 19:37:58 +0300

    test

--- B19_Ex01_05/Program.cs
+++ B19_Ex01_05/Program.cs
@@ -15,7 +15,45 @@ namespace B19_Ex01_05
              * private static int biggestDigitInString(string i_input);
              * private static int smallestDigitInString(string i_input);
              * 
-             * /
+             */
         }
+
+        private static string getInputFromUser()
+        {
+            string i_input;
+
+            Console.WriteLine("Please insert a 8 digit positive number");
+            do
+            {
+                i_input = Console.ReadLine();
+            } while (!checkUserInput(i_input));
+
+            return i_input;
+        }
+
+        private static bool checkUserInput(string i_input)
+        {
+            int i_decimalInput;
+
+            if (i_input.Length != 8)
+            {
+                Console.WriteLine("Your input is different than 8 digits, please try again");
+                return false;
+            }
+            else if(!Int32.TryParse(i_input, out i_decimalInput))
+            {
+                Console.WriteLine("Your input is not a number, please try again");
+                return false;
+            }
+            else if(i_decimalInput <= 0)
+            {
+                Console.WriteLine("Your input is not positive, please try again");
+                return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
